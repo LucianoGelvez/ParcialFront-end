32ef1eb3deee6a07d5b48e2dd91719cf9a0b456a
Luciano Gelvez <Lucianomagallanes55@gmail.com>
2023-03-07 07:50:06 -0300

    pre-parcial

---
+++ Parcial1/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Card from './componentes/Card'
+
+
+function App() {
+  return (
+    <div>
+     <Card></Card>
+     <form></form>
+    </div>
+  )
+}
+
+export default App
---
+++ Parcial1/src/componentes/Form.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+const Form = () => {
+    const [user, setUser] = useState({
+        name: "",
+        obraSocial: "",
+        email: "",
+        medico: ""
+    })
+return (
+    <div>
+        <h1></h1>
+        <select name="" id="">
+           <opcion></opcion>
+           <opcion></opcion>
+           <opcion></opcion>
+        </select>
+    </div>
+)
+}
+
+export default Form
